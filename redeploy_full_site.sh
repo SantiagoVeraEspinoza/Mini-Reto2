e3308ba58bac743c1db133348bd9f8106897753f
TitanPro47 <santiveraespinoza@gmail.com>
2023-04-29 19:11:57 -0600

    pwd

--- redeploy_full_site.sh
+++ redeploy_full_site.sh
@@ -13,10 +13,12 @@ tmux new-session -s my-shop-api -d
 tmux send-keys -t my-shop-api:0 "cd /MyShop" C-m
 tmux send-keys -t my-shop-api:0 "npm i" C-m
 tmux send-keys -t my-shop-api:0 "npm start" C-m
+tmux send-keys -t my-shop-api:0 "pwd" C-m
 
 tmux new-session -s my-shop-site -d
 tmux send-keys -t my-shop-site:0 "cd /Myshop-remix" C-m
 tmux send-keys -t my-shop-site:0 "npm i" C-m
 tmux send-keys -t my-shop-site:0 "npm run dev" C-m
+tmux send-keys -t my-shop-site:0 "pwd" C-m
 
 cd $WD
