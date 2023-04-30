514ed9dfb55cc7ed5eed09f4ddf06b3c7d928fbd
TitanPro47 <santiveraespinoza@gmail.com>
2023-04-29 19:09:06 -0600

    correccion 2

--- redeploy_full_site.sh
+++ redeploy_full_site.sh
@@ -10,13 +10,13 @@ git reset origin/main --hard
 tmux kill-server
 
 tmux new-session -s my-shop-api -d
-tmux send-keys -t my-shop-api:1 "cd /MyShop" C-m
-tmux send-keys -t my-shop-api:1 "npm i" C-m
-tmux send-keys -t my-shop-api:1 "npm start" C-m
+tmux send-keys -t my-shop-api:0 "cd /MyShop" C-m
+tmux send-keys -t my-shop-api:0 "npm i" C-m
+tmux send-keys -t my-shop-api:0 "npm start" C-m
 
 tmux new-session -s my-shop-site -d
-tmux send-keys -t my-shop-api:1 "cd /Myshop-remix" C-m
-tmux send-keys -t my-shop-site:1 "npm i" C-m
-tmux send-keys -t my-shop-site:1 "npm run dev" C-m
+tmux send-keys -t my-shop-site:0 "cd /Myshop-remix" C-m
+tmux send-keys -t my-shop-site:0 "npm i" C-m
+tmux send-keys -t my-shop-site:0 "npm run dev" C-m
 
 cd $WD
